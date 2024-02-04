1e1ef2eb177cc1c83fa5e4aa6481c373c75f8af2
HenriqueFragaRosa <hf.rosa002@gmail.com>
2024-02-04 02:21:07 -0300

    feat: add functions set and get

--- entities/Triangle.java
+++ entities/Triangle.java
@@ -1,17 +1,31 @@
+//This is the Triangle class that contains the height and base of the triangle and the method to calculate the area of the triangle.
 package entities;
 
 public class Triangle {
-    public double a;
-    public double b;
-    public double c;
+    public double height;
+    public double base;
 
     public double area() {
-        double p = (a + b + c) / 2.0;
-        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return (base * height) / 2;
     }
 
     public String toString() {
-        return String.format("Area: %.4f", area());
+        return String.format("Area: %.2f", area());
     }
 
+    public Double getHeight() {
+        return height;
+    }
+
+    public Double getBase() {
+        return base;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public void setBase(Double base) {
+        this.base = base;
+    }
 }
